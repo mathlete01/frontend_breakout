f5ea8ce9a74f31dfc494896e3a04b3f8ebdb3845
mathlete01 <msallin@gmail.com>
2020-12-07 16:16:37 -0800

    Leaderboard, game, and text entry all appear as separate screens

--- interface.js
+++ interface.js
@@ -5,9 +5,270 @@ const interface = document.getElementById("interface");
 const leaderboard = document.getElementById("leaderboard");
 const browser = navigator.appName;
 const platform = navigator.platform;
-
+const w = window.innerWidth;
+const h = window.innerHeight;
+let speed = 0.9;
+let lives = 1;
+let canvas = document.getElementById("myCanvas");
+let keySegments = 15;
+const factor = 5 / keySegments;
+canvas.width = w;
+canvas.height = factor * w;
+let ctx = canvas.getContext("2d");
+let ballRadius = 10;
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = speed;
+let dy = -1 * dx;
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let rightPressed = false;
+let leftPressed = false;
+let keyRowCount = 5;
+let keyColumnCount = 15;
+let keyPadding = 10;
+let keyOffsetTop = 30;
+let keyOffsetLeft = 30;
+let keyWidth = w / keySegments;
+let keyHeight = keyWidth;
+let topRow = 100;
+let interval = "";
+let score = 0;
+const row0 = [
+  {
+    name: "`",
+    code: "Backquote",
+    row: 0,
+    segments: 1,
+    status: 0,
+    position: 0,
+  },
+  { name: "1", code: "Digit1", row: 0, segments: 1, status: 0, position: 1 },
+  { name: "2", code: "Digit2", row: 0, segments: 1, status: 0, position: 2 },
+  { name: "3", code: "Digit3", row: 0, segments: 1, status: 0, position: 3 },
+  { name: "4", code: "Digit4", row: 0, segments: 1, status: 0, position: 4 },
+  { name: "5", code: "Digit5", row: 0, segments: 1, status: 0, position: 5 },
+  { name: "6", code: "Digit6", row: 0, segments: 1, status: 0, position: 6 },
+  { name: "7", code: "Digit7", row: 0, segments: 1, status: 0, position: 7 },
+  { name: "8", code: "Digit8", row: 0, segments: 1, status: 0, position: 8 },
+  { name: "9", code: "Digit9", row: 0, segments: 1, status: 0, position: 9 },
+  { name: "0", code: "Digit0", row: 0, segments: 1, status: 0, position: 10 },
+  { name: "-", code: "Minus", row: 0, segments: 1, status: 0, position: 11 },
+  { name: "=", code: "Equal", row: 0, segments: 1, status: 0, position: 12 },
+  {
+    name: "Backspace",
+    code: "Backspace",
+    row: 0,
+    segments: 2,
+    status: 0,
+    position: 13,
+  },
+];
+const row1 = [
+  { name: "Tab", code: "Tab", row: 1, segments: 2, status: 0, position: 0 },
+  { name: "q", code: "KeyQ", row: 1, segments: 1, status: 0, position: 2 },
+  { name: "w", code: "KeyW", row: 1, segments: 1, status: 0, position: 3 },
+  { name: "e", code: "KeyE", row: 1, segments: 1, status: 0, position: 4 },
+  { name: "r", code: "KeyR", row: 1, segments: 1, status: 0, position: 5 },
+  { name: "t", code: "KeyT", row: 1, segments: 1, status: 0, position: 6 },
+  { name: "y", code: "KeyY", row: 1, segments: 1, status: 0, position: 7 },
+  { name: "u", code: "KeyU", row: 1, segments: 1, status: 0, position: 8 },
+  { name: "i", code: "KeyI", row: 1, segments: 1, status: 0, position: 9 },
+  { name: "o", code: "KeyO", row: 1, segments: 1, status: 0, position: 10 },
+  { name: "p", code: "KeyP", row: 1, segments: 1, status: 0, position: 11 },
+  {
+    name: "[",
+    code: "BracketLeft",
+    row: 1,
+    segments: 1,
+    status: 0,
+    position: 12,
+  },
+  {
+    name: "]",
+    code: "BracketRight",
+    row: 1,
+    segments: 1,
+    status: 0,
+    position: 13,
+  },
+  {
+    name: "\\",
+    code: "Backslash",
+    row: 1,
+    segments: 1,
+    status: 0,
+    position: 14,
+  },
+];
+const row2 = [
+  {
+    name: "CapsLock",
+    code: "CapsLock",
+    row: 2,
+    segments: 2,
+    status: 0,
+    position: 0,
+  },
+  { name: "a", code: "KeyA", row: 2, segments: 1, status: 0, position: 2 },
+  { name: "s", code: "KeyS", row: 2, segments: 1, status: 0, position: 3 },
+  { name: "d", code: "KeyD", row: 2, segments: 1, status: 0, position: 4 },
+  { name: "f", code: "KeyF", row: 2, segments: 1, status: 0, position: 5 },
+  { name: "g", code: "KeyG", row: 2, segments: 1, status: 0, position: 6 },
+  { name: "h", code: "KeyH", row: 2, segments: 1, status: 0, position: 7 },
+  { name: "j", code: "KeyJ", row: 2, segments: 1, status: 0, position: 8 },
+  { name: "k", code: "KeyK", row: 2, segments: 1, status: 0, position: 9 },
+  { name: "l", code: "KeyL", row: 2, segments: 1, status: 0, position: 10 },
+  {
+    name: ":",
+    code: "Semicolon",
+    row: 2,
+    segments: 1,
+    status: 0,
+    position: 11,
+  },
+  { name: "'", code: "Quote", row: 2, segments: 1, status: 0, position: 12 },
+  {
+    name: "Enter",
+    code: "Enter",
+    row: 2,
+    segments: 2,
+    status: 0,
+    position: 13,
+  },
+];
+const row3 = [
+  {
+    name: "Shift",
+    code: "ShiftLeft",
+    row: 3,
+    segments: 2.5,
+    status: 0,
+    position: 0,
+  },
+  { name: "z", code: "KeyZ", row: 3, segments: 1, status: 0, position: 2.5 },
+  { name: "x", code: "KeyX", row: 3, segments: 1, status: 0, position: 3.5 },
+  { name: "c", code: "KeyC", row: 3, segments: 1, status: 0, position: 4.5 },
+  { name: "v", code: "KeyV", row: 3, segments: 1, status: 0, position: 5.5 },
+  { name: "b", code: "KeyB", row: 3, segments: 1, status: 0, position: 6.5 },
+  { name: "n", code: "KeyN", row: 3, segments: 1, status: 0, position: 7.5 },
+  { name: "m", code: "KeyM", row: 3, segments: 1, status: 0, position: 8.5 },
+  { name: ",", code: "Comma", row: 3, segments: 1, status: 0, position: 9.5 },
+  {
+    name: ".",
+    code: "Period",
+    row: 3,
+    segments: 1,
+    status: 0,
+    position: 10.5,
+  },
+  {
+    name: "/",
+    code: "Slash",
+    row: 3,
+    segments: 1,
+    status: 0,
+    position: 11.5,
+  },
+  {
+    name: "Shift",
+    code: "ShiftRight",
+    row: 3,
+    segments: 2.5,
+    status: 0,
+    position: 12.5,
+  },
+];
+const row4 = [
+  { name: "Function", code: "", row: 4, segments: 1, status: 0, position: 0 },
+  {
+    name: "Control",
+    code: "ControlLeft",
+    row: 4,
+    segments: 1,
+    status: 0,
+    position: 1,
+  },
+  {
+    name: "Alt",
+    code: "AltLeft",
+    row: 4,
+    segments: 1,
+    status: 0,
+    position: 2,
+  },
+  {
+    name: "⌘",
+    code: "MetaLeft",
+    row: 4,
+    segments: 1.5,
+    status: 0,
+    position: 3,
+  },
+  {
+    name: "Space",
+    code: "Space",
+    row: 4,
+    segments: 5,
+    status: 0,
+    position: 4.5,
+  },
+  {
+    name: "⌘",
+    code: "MetaRight",
+    row: 4,
+    segments: 1.5,
+    status: 0,
+    position: 9.5,
+  },
+  {
+    name: "Alt",
+    code: "AltRight",
+    row: 4,
+    segments: 1,
+    status: 0,
+    position: 11,
+  },
+  {
+    name: "←",
+    code: "ArrowLeft",
+    row: 4,
+    segments: 1,
+    status: 0,
+    position: 12,
+  },
+  {
+    name: "↑",
+    code: "ArrowUp",
+    row: 4,
+    segments: 1,
+    status: 0,
+    position: 13,
+  },
+  {
+    name: "↓",
+    code: "ArrowDown",
+    row: 4,
+    segments: 1,
+    status: 0,
+    position: 13,
+  },
+  {
+    name: "→",
+    code: "ArrowRight",
+    row: 4,
+    segments: 1,
+    status: 0,
+    position: 14,
+  },
+];
 
 document.addEventListener("DOMContentLoaded", (event) => {
+  function bringToFront(obj) {
+    obj.style.zIndex = "1";
+  }
+
   function setCurrentPlayer(obj) {
     CURRENT_PLAYER = obj.id;
     console.log(`CURRENT_PLAYER = ${CURRENT_PLAYER}`);
@@ -36,7 +297,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   function createGame(id) {
     console.log(`createGame id = ${id}`);
-    leaderboard.innerHTML = ""
+    leaderboard.innerHTML = "";
     let formData = {
       player_id: id,
     };
@@ -142,42 +403,43 @@ document.addEventListener("DOMContentLoaded", (event) => {
       .catch((errors) => console.log(`updateGame: ${errors}`));
   }
 
-  function getLeaderboard(){
+  function getLeaderboard() {
     fetch("http://localhost:3000/games")
-    .then((res) => res.json())
-    .then((json) => {
-      const objs = json;
-      renderLeaderboard(objs);
-    });
+      .then((res) => res.json())
+      .then((json) => {
+        const objs = json;
+        renderLeaderboard(objs);
+      });
   }
 
-  getLeaderboard()
+  getLeaderboard();
 
   function renderLeaderboard(arr) {
-    arr.sort((a, b) => (a.score < b.score) ? 1 : -1)
-    let h1 = document.createElement("h1")
-    h1.innerText="Top Ten Scores"
+    arr.sort((a, b) => (a.score < b.score ? 1 : -1));
+    let h1 = document.createElement("h1");
+    h1.innerText = "Top Ten Scores";
     let ol = document.createElement("ol");
-    ol.setAttribute("padding-left", 30)
-    ol.setAttribute("list-style-position", "inside")
-    ol.setAttribute("margin-left", "20px")
-    for(let i = 0; i < 10; i ++){
+    ol.setAttribute("padding-left", 30);
+    ol.setAttribute("list-style-position", "inside");
+    ol.setAttribute("margin-left", "20px");
+    for (let i = 0; i < 10; i++) {
       let li = document.createElement("li");
-      li.setAttribute("display", "list-item")
-      li.setAttribute("list-style-position", "inside")
-      let element = arr[i]
-      let s = element["score"]
-      let p = element["player"]["name"]
+      li.setAttribute("display", "list-item");
+      li.setAttribute("list-style-position", "inside");
+      let element = arr[i];
+      let s = element["score"];
+      let p = element["player"]["name"];
       li.innerText = `${p}......${s} points`;
       ol.append(li);
     }
-    leaderboard.append(h1)
+    leaderboard.append(h1);
     leaderboard.append(ol);
     const btnStart = document.createElement("button");
     btnStart.setAttribute("id", "btn-start");
     btnStart.innerHTML = "Start Game";
     btnStart.addEventListener("click", () => createPlayer());
     leaderboard.append(btnStart);
+    bringToFront(leaderboard)
   }
 
   function renderForm() {
@@ -195,13 +457,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const form = document.getElementById("form");
     form.append(playername);
     form.append(btnSave);
-  }
-
-  function renderGameboard() {
-    ctx.width = window.innerWidth;
-    ctx.height = window.innerHeight;
-    console.log(`ctx.width = ${ctx.width}`);
-    console.log(`window.innerWidth = ${window.innerWidth}`);
+    bringToFront(form)
   }
 
   function activateKeyListeners() {
@@ -305,13 +561,21 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }
   }
 
+  function drawOutlines(array) {
+    for (let i = 0; i < array.length; i++) {
+      let key = array[i];
+      drawKeyOutline(key.name, key.code, key.x, key.y, key.w, key.h);
+    }
+  }
+
   function drawKeyOutline(name, code, keyX, keyY, keyWidth, keyHeight) {
-    //console.log(`Drawing = ${name}`)
+    console.log(`Drawing = ${name}`);
     ctx.beginPath();
     ctx.rect(keyX, keyY, keyWidth, keyHeight);
     ctx.stroke();
     ctx.closePath();
     ctx.id = code;
+    ctx.strokeStyle = "#d9d9d9";
     //ctx.font = "16px Arial";
     //ctx.fillStyle = "red";
     //ctx.fillText(name, keyX + keyWidth / 2, keyY + keyHeight / 2);
@@ -352,7 +616,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.lineWidth = 2;
+    ctx.lineWidth = 1;
     ctx.strokeStyle = "#000000";
     ctx.strokeRect(0, 0, canvas.width, canvas.height);
     //ctx.fillStyle = "#0000FF"
@@ -400,262 +664,18 @@ document.addEventListener("DOMContentLoaded", (event) => {
     y += dy;
   }
 
-  const w = window.innerWidth;
-  const h = window.innerHeight;
-  let speed = 0.9;
-  let lives = 1;
-  let canvas = document.getElementById("myCanvas");
-  let keySegments = 15;
-  const factor = 5 / keySegments;
-  canvas.width = w;
-  canvas.height = factor * w;
-  let ctx = canvas.getContext("2d");
-  let ballRadius = 10;
-  let x = canvas.width / 2;
-  let y = canvas.height - 30;
-  let dx = speed;
-  let dy = -1 * dx;
-  let paddleHeight = 10;
-  let paddleWidth = 75;
-  let paddleX = (canvas.width - paddleWidth) / 2;
-  let rightPressed = false;
-  let leftPressed = false;
-  let keyRowCount = 5;
-  let keyColumnCount = 15;
-  let keyPadding = 10;
-  let keyOffsetTop = 30;
-  let keyOffsetLeft = 30;
-  let keyWidth = w / keySegments;
-  let keyHeight = keyWidth;
-  let topRow = 100;
-  let interval = "";
-  let score = 0;
-  const row0 = [
-    {
-      name: "`",
-      code: "Backquote",
-      row: 0,
-      segments: 1,
-      status: 0,
-      position: 0,
-    },
-    { name: "1", code: "Digit1", row: 0, segments: 1, status: 0, position: 1 },
-    { name: "2", code: "Digit2", row: 0, segments: 1, status: 0, position: 2 },
-    { name: "3", code: "Digit3", row: 0, segments: 1, status: 0, position: 3 },
-    { name: "4", code: "Digit4", row: 0, segments: 1, status: 0, position: 4 },
-    { name: "5", code: "Digit5", row: 0, segments: 1, status: 0, position: 5 },
-    { name: "6", code: "Digit6", row: 0, segments: 1, status: 0, position: 6 },
-    { name: "7", code: "Digit7", row: 0, segments: 1, status: 0, position: 7 },
-    { name: "8", code: "Digit8", row: 0, segments: 1, status: 0, position: 8 },
-    { name: "9", code: "Digit9", row: 0, segments: 1, status: 0, position: 9 },
-    { name: "0", code: "Digit0", row: 0, segments: 1, status: 0, position: 10 },
-    { name: "-", code: "Minus", row: 0, segments: 1, status: 0, position: 11 },
-    { name: "=", code: "Equal", row: 0, segments: 1, status: 0, position: 12 },
-    {
-      name: "Backspace",
-      code: "Backspace",
-      row: 0,
-      segments: 2,
-      status: 0,
-      position: 13,
-    },
-  ];
-  const row1 = [
-    { name: "Tab", code: "Tab", row: 1, segments: 2, status: 0, position: 0 },
-    { name: "q", code: "KeyQ", row: 1, segments: 1, status: 0, position: 2 },
-    { name: "w", code: "KeyW", row: 1, segments: 1, status: 0, position: 3 },
-    { name: "e", code: "KeyE", row: 1, segments: 1, status: 0, position: 4 },
-    { name: "r", code: "KeyR", row: 1, segments: 1, status: 0, position: 5 },
-    { name: "t", code: "KeyT", row: 1, segments: 1, status: 0, position: 6 },
-    { name: "y", code: "KeyY", row: 1, segments: 1, status: 0, position: 7 },
-    { name: "u", code: "KeyU", row: 1, segments: 1, status: 0, position: 8 },
-    { name: "i", code: "KeyI", row: 1, segments: 1, status: 0, position: 9 },
-    { name: "o", code: "KeyO", row: 1, segments: 1, status: 0, position: 10 },
-    { name: "p", code: "KeyP", row: 1, segments: 1, status: 0, position: 11 },
-    {
-      name: "[",
-      code: "BracketLeft",
-      row: 1,
-      segments: 1,
-      status: 0,
-      position: 12,
-    },
-    {
-      name: "]",
-      code: "BracketRight",
-      row: 1,
-      segments: 1,
-      status: 0,
-      position: 13,
-    },
-    {
-      name: "\\",
-      code: "Backslash",
-      row: 1,
-      segments: 1,
-      status: 0,
-      position: 14,
-    },
-  ];
-  const row2 = [
-    {
-      name: "CapsLock",
-      code: "CapsLock",
-      row: 2,
-      segments: 2,
-      status: 0,
-      position: 0,
-    },
-    { name: "a", code: "KeyA", row: 2, segments: 1, status: 0, position: 2 },
-    { name: "s", code: "KeyS", row: 2, segments: 1, status: 0, position: 3 },
-    { name: "d", code: "KeyD", row: 2, segments: 1, status: 0, position: 4 },
-    { name: "f", code: "KeyF", row: 2, segments: 1, status: 0, position: 5 },
-    { name: "g", code: "KeyG", row: 2, segments: 1, status: 0, position: 6 },
-    { name: "h", code: "KeyH", row: 2, segments: 1, status: 0, position: 7 },
-    { name: "j", code: "KeyJ", row: 2, segments: 1, status: 0, position: 8 },
-    { name: "k", code: "KeyK", row: 2, segments: 1, status: 0, position: 9 },
-    { name: "l", code: "KeyL", row: 2, segments: 1, status: 0, position: 10 },
-    {
-      name: ":",
-      code: "Semicolon",
-      row: 2,
-      segments: 1,
-      status: 0,
-      position: 11,
-    },
-    { name: "'", code: "Quote", row: 2, segments: 1, status: 0, position: 12 },
-    {
-      name: "Enter",
-      code: "Enter",
-      row: 2,
-      segments: 2,
-      status: 0,
-      position: 13,
-    },
-  ];
-  const row3 = [
-    {
-      name: "Shift",
-      code: "ShiftLeft",
-      row: 3,
-      segments: 2.5,
-      status: 0,
-      position: 0,
-    },
-    { name: "z", code: "KeyZ", row: 3, segments: 1, status: 0, position: 2.5 },
-    { name: "x", code: "KeyX", row: 3, segments: 1, status: 0, position: 3.5 },
-    { name: "c", code: "KeyC", row: 3, segments: 1, status: 0, position: 4.5 },
-    { name: "v", code: "KeyV", row: 3, segments: 1, status: 0, position: 5.5 },
-    { name: "b", code: "KeyB", row: 3, segments: 1, status: 0, position: 6.5 },
-    { name: "n", code: "KeyN", row: 3, segments: 1, status: 0, position: 7.5 },
-    { name: "m", code: "KeyM", row: 3, segments: 1, status: 0, position: 8.5 },
-    { name: ",", code: "Comma", row: 3, segments: 1, status: 0, position: 9.5 },
-    {
-      name: ".",
-      code: "Period",
-      row: 3,
-      segments: 1,
-      status: 0,
-      position: 10.5,
-    },
-    {
-      name: "/",
-      code: "Slash",
-      row: 3,
-      segments: 1,
-      status: 0,
-      position: 11.5,
-    },
-    {
-      name: "Shift",
-      code: "ShiftRight",
-      row: 3,
-      segments: 2.5,
-      status: 0,
-      position: 12.5,
-    },
-  ];
-  const row4 = [
-    { name: "Function", code: "", row: 4, segments: 1, status: 0, position: 0 },
-    {
-      name: "Control",
-      code: "ControlLeft",
-      row: 4,
-      segments: 1,
-      status: 0,
-      position: 1,
-    },
-    {
-      name: "Alt",
-      code: "AltLeft",
-      row: 4,
-      segments: 1,
-      status: 0,
-      position: 2,
-    },
-    {
-      name: "⌘",
-      code: "MetaLeft",
-      row: 4,
-      segments: 1.5,
-      status: 0,
-      position: 3,
-    },
-    {
-      name: "Space",
-      code: "Space",
-      row: 4,
-      segments: 5,
-      status: 0,
-      position: 4.5,
-    },
-    {
-      name: "⌘",
-      code: "MetaRight",
-      row: 4,
-      segments: 1.5,
-      status: 0,
-      position: 9.5,
-    },
-    {
-      name: "Alt",
-      code: "AltRight",
-      row: 4,
-      segments: 1,
-      status: 0,
-      position: 11,
-    },
-    {
-      name: "←",
-      code: "ArrowLeft",
-      row: 4,
-      segments: 1,
-      status: 0,
-      position: 12,
-    },
-    {
-      name: "↑",
-      code: "ArrowUp",
-      row: 4,
-      segments: 1,
-      status: 0,
-      position: 13,
-    },
-    {
-      name: "↓",
-      code: "ArrowDown",
-      row: 4,
-      segments: 1,
-      status: 0,
-      position: 13,
-    },
-    {
-      name: "→",
-      code: "ArrowRight",
-      row: 4,
-      segments: 1,
-      status: 0,
-      position: 14,
-    },
-  ];
+  function renderGameboard() {
+    ctx.width = window.innerWidth;
+    ctx.height = window.innerHeight;
+    console.log(`ctx.width = ${ctx.width}`);
+    console.log(`window.innerWidth = ${window.innerWidth}`);
+    initKeys(row0);
+    initKeys(row1);
+    initKeys(row2);
+    initKeys(row3);
+    initKeys(row4);
+    drawOutlines(KEY_ARRAY);
+  }
+
+  renderGameboard();
 });
